3d9f3d9346d4b7f4c02ec29e67af4498bb30b296
KristianYanakov <christianyanakov@gmail.com>
2025-03-18 18:53:43 -0700

    added a player paddle and a grid

--- game.js
+++ game.js
@@ -1,14 +1,54 @@
 ﻿// Creating variables
-var myX = 0, myY = 0;
+//var myX = 0, myY = 0;
+
+endlessCanvas = true
+
+var width = 1000;
+var height = 700;
+
+var player = {x: width/2, y: 600, sx: 150, sy: 30}
+
+var ball = {x: 0, y: 300, direction: "up", speed: 3}
+
+var grid = [], numX = 5, numY = 3, tile = {sx: 100, sy: 30};
+var padding = 1, margin = 100
+
+for(let i = 0; i < numX; i ++){
+    grid[i] = []
+    for(let j = 0; j < numY; j ++){
+        grid[i][j] = 1
+    }
+}
 
 function update() {
-    myX = myX+(mouseX-myX)/10;
-    myY = myY+(mouseY-myY)/10;
+    //myX = myX+(mouseX-myX)/10;
+    //myY = myY+(mouseY-myY)/10;
+
+    //Introduce player movement and border control
+    player.x = mouseX - player.sx/2
+
+    if(player.x <= 0){
+        player.x = 0
+    }
+    if((player.x + player.sx) >= width){
+        player.x = width - player.sx
+    }
+
+    //Ball Movement
+    
 }
 
 function draw() {
     // This is how you draw a rectangle
-    context.fillRect(myX, myY, 30, 30);
+    context.strokeRect(0, 0, width, height);
+    context.fillRect(player.x, player.y, player.sx, player.sy);
+
+  
+    for(let i = 0; i < numX; i ++){
+        for(let j = 0; j < numY; j ++){
+            context.fillRect(i * (tile.sx + padding) + margin, j * (tile.sy + padding) + margin, tile.sx, tile.sy);
+        }
+    }
 };
 
 function keyup(key) {
